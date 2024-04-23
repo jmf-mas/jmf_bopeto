9c0fd3ce692bcf9d12c32ee183872d3ceabdc04d
jmf-mas <masakunafelicien@gmail.com>
2024-04-23 11:57:57 -0400

    main bash

--- main.sh
+++ main.sh
@@ -4,25 +4,20 @@ datasets=("ciciot" "credit" "ecg" "ids" "kdd" "kitsune")
 models=("ae" "alad" "dagmm" "dsebm" "svdd", "if", "lof", "ocsvm")
 datasets=("nsl")
 models=("ae")
-
-python_file1="cleaning.py"
-python_file2="detection.py"
-name="--name"
-method="--model"
-
-# Define a function to process each dataset and model
 process_dataset_model() {
+    cleaning="cleaning.py"
+    detection="detection.py"
+    name="--name"
+    method="--model"
     dataset="$1"
     model="$2"
     echo "Processing dataset: $dataset with model: $model"
-    python "$python_file1" "$name" "$dataset"
-    python "$python_file2" "$name" "$dataset" "$method" "$model"
+    python "$cleaning" "$name" "$dataset"
+    python "$detection" "$name" "$dataset" "$method" "$model"
     echo "Finished processing dataset: $dataset with model: $model"
 }
 
-# Export the function so GNU Parallel can access it
 export -f process_dataset_model
 
-# Run the function in parallel for each dataset and model
 parallel -j "$(nproc)" process_dataset_model ::: "${datasets[@]}" ::: "${models[@]}"
 echo "All datasets and models processed successfully"
